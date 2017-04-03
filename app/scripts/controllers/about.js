907496f1a6bede29c1f8e70549f9aca44c62ea48
Andrei Cristian Petcu <andrei@ceata.org>
2017-04-03 21:59:30 +0300

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twhApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the twhApp
+ */
+angular.module('twhApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twhApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twhApp
+ */
+angular.module('twhApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
